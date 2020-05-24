3835c21a32c4e357366391e524089b5375645539
Victor A <victor@namespace.ee>
2020-05-24 14:47:18 +0300

    fix travis config

--- worker/index.js
+++ worker/index.js
@@ -14,6 +14,6 @@ function fib(index) {
 }
 
 sub.on('message', (channel, message) => {
-  redisClient.set('values', message, fib(parseInt(message)));
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
 sub.subscribe('insert');
